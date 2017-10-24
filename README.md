9d24d5a5ab1931dab7901412a20e6cda2faa4a3b
pritwiraj sinha <pritwiraj.sinha@oracle.com>
2017-10-24 09:02:09 -0700

    First Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Docker-ReverseProxy.git
+
---
+++ build.sh
@@ -0,0 +1,2 @@
+docker build -t oc-129-157-181-103.compute.oraclecloud.com/reverse-proxy:master .
+docker push oc-129-157-181-103.compute.oraclecloud.com/reverse-proxy:master
